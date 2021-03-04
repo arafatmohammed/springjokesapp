87063b63ea142a95b064b3a5c4e328718acb0def
Arafat Mohammed <arafat@dais.com>
2021-03-03 21:52:28 -0600

    Implemented Spring XML Configuration instead of using Bean from last commit

--- src/main/java/com/amdcloudguru/springjokesapp/config/ChuckConfiguration.java
+++ src/main/java/com/amdcloudguru/springjokesapp/config/ChuckConfiguration.java
@@ -2,13 +2,11 @@ package com.amdcloudguru.springjokesapp.config;
 
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// @Configuration
 public class ChuckConfiguration {
 
-    @Bean
+    // @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
